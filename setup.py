a6a5bc925a0035e232926ab67ec802d7fa514b60
phonchi <steve2003121@gmail.com>
2025-06-18 17:09:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='CryoParticleSeg',
-    version='0.0.1',
+    version='0.1.0',
     description="A particle picking pipeline for cryo-EM micrographs using semantic segmentation trained by projection of reconstructed 3D volume.",
-    author="Po-Cheng Chou",
-    author_email="cyanazuki@gmail.com",
-    url="https://github.com/cyanazuki/CryoParticleSegment",
+    author="Po-Cheng Chou, Szu-Chi Chung",
+    author_email="steve2003121@gmail.com",
+    url="https://github.com/phonchi/CryoParticleSegment",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",

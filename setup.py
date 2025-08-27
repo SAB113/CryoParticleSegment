4115d638328a05f161446144d0c5c9099febdbaf
phonchi <steve2003121@gmail.com>
2025-08-27 10:34:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='CryoParticleSeg',
     version='0.1.0',
-    description="A particle picking pipeline for cryo-EM micrographs using semantic segmentation trained by projection of reconstructed 3D volume.",
-    author="Po-Cheng Chou, Szu-Chi Chung",
+    description="CRISP: A Modular Platform for Cryo-EM Image Segmentation and Processing with Conditional Random Field",
+    author="Szu-Chi Chung and Po-Cheng Chou",
     author_email="steve2003121@gmail.com",
     url="https://github.com/phonchi/CryoParticleSegment",
     classifiers=[
@@ -15,3 +15,4 @@ setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
+
